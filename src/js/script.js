421443caf931ba2e65da0ece11e1ef5a43276a04
Ivo Gregurec <ivo@toptal.com>
2016-10-07 02:09:27 +0200

    transitions and mobile view

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    console.log("wtf");
+    $('.slider').owlCarousel( {
+	  autoPlay: true,
+	  paginationSpeed : 6000,
+	  singleItem: true,
+	  transitionStyle : "fadeUp"
+	});
+    
+});
