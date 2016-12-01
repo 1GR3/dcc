4e6a60988313f05311930b67583c5a89f8afe9ec
Ivo Gregurec <ivo@toptal.com>
2016-12-01 20:49:15 +0100

    a lot of minor changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,16 @@
 $( document ).ready(function() {
+
+	// burger menu
+	
+	$(".menu-icon").click(function(){
+	    $(".main-nav ul").toggleClass("open");
+    });
+    $(".main-nav ul").click(function(){
+	    $(this).toggleClass("open");
+    });
+	
+	// landingpage  //
+	
     console.log("wtf");
     $('.slider').owlCarousel( {
 	  autoPlay: true,
@@ -8,8 +20,79 @@ $( document ).ready(function() {
 	});
     
 	$('.pictures').slick({
-	    setting-name: setting-value
+	    //setting-name: setting-value
 	});
 	
-	console.log("olala")
+
+	
+	
+	
+	
+	
+	// single property page //
+	
+	$().ready(function (e) {
+	    $('.slickslide').slick({
+	        dots: true,
+	        infinite: true,
+	        speed: 500,
+	        fade: false,
+	        slide: 'li',
+	        cssEase: 'linear',
+	        centerMode: true,
+	        slidesToShow: 1,
+	        variableWidth: true,
+	        autoplay: true,
+	        autoplaySpeed: 4000,
+	        responsive: [{
+	            breakpoint: 800,
+	            settings: {
+	                arrows: false,
+	                centerMode: false,
+	                centerPadding: '40px',
+	                variableWidth: false,
+	                slidesToShow: 1,
+	                dots: true
+	            },
+	            breakpoint: 1024,
+	            settings: {
+	                arrows: false,
+	                centerMode: false,
+	                centerPadding: '40px',
+	                variableWidth: false,
+	                slidesToShow: 1,
+	                dots: true
+	
+	            }
+	        }],
+	        customPaging: function (slider, i) {
+	            return '<button class="tab">' + $('.slick-thumbs li:nth-child(' + (i + 1) + ')').html() + '</button>';
+	        }
+	    });
+	});
+	
+	$(".tab-nav li").click(function(){
+    	$(this).siblings().removeClass("active");
+    	$(this).addClass("active");
+	})
+	$(".t1").click(function(){
+		$(".tab-content").removeClass("active");
+    	$(".tc-1").addClass("active");
+    	
+	})
+	$(".t2").click(function(){
+		$(".tab-content").removeClass("active");
+    	$(".tc-2").addClass("active");
+    	
+	})
+	$(".t3").click(function(){
+		$(".tab-content").removeClass("active");
+    	$(".tc-3").addClass("active");
+    	
+	})
+	$(".t4").click(function(){
+		$(".tab-content").removeClass("active");
+    	$(".tc-4").addClass("active");
+    	
+	})
 });
